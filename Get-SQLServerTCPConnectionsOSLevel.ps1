1974defe098efbefd43c529863bef14234181d62
RATG <88596567+ragatilao@users.noreply.github.com>
2024-03-31 08:40:07 -0500

    Update Get-SQLServerTCPConnectionsOSLevel.ps1

--- Get-SQLServerTCPConnectionsOSLevel.ps1
+++ Get-SQLServerTCPConnectionsOSLevel.ps1
@@ -36,13 +36,13 @@ function Get-SQLServerTCPConnectionsOSLevel
                 $TCPLocalAddress = $TCPConnectionDetail.LocalAddress 
 
                 $LocalHostName = $null
-                $LocalHostName = Resolve-DnsName "$TCPLocalAddress" -DnsOnly | Select-Object -ExpandProperty NameHost -ErrorAction SilentlyContinue
+                $LocalHostName = Resolve-DnsName "$TCPLocalAddress" -ErrorAction SilentlyContinue -DnsOnly | Select-Object -ExpandProperty NameHost
 
                 $TCPRemoteAddress = $null
                 $TCPRemoteAddress = $TCPConnectionDetail.RemoteAddress 
 
                 $RemoteHostName = $null
-                $RemoteHostName = Resolve-DnsName "$TCPRemoteAddress" -DnsOnly | Select-Object -ExpandProperty NameHost -ErrorAction SilentlyContinue
+                $RemoteHostName = Resolve-DnsName "$TCPRemoteAddress" -ErrorAction SilentlyContinue -DnsOnly | Select-Object -ExpandProperty NameHost
         
                 $SQLServerProcessDetail = New-Object -TypeName PSObject 
                 $SQLServerProcessDetail | Add-Member -MemberType NoteProperty -Name ProcessName -Value $SQLServerProcessName
